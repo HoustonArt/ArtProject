adfb68f8b58b4bbdf9d6fa080ca2379f05c246aa
zachglassman <zach.glassman@gmail.com>
2016-08-05 22:59:59 -0400

    site update

--- utils/add_database_field.py
+++ utils/add_database_field.py
@@ -1,14 +1,14 @@
 import pyrebase
 import argparse
+import traceback
 
 config = {
     'apiKey': "AIzaSyAZ35SfNAcOJV7Jc6jbsofyJLhigV8R52c",
     'authDomain': "artlike.firebaseapp.com",
     'databaseURL': "https://artlike.firebaseio.com",
     'storageBucket': "firebase-artlike.appspot.com",
+    #"serviceAccount":"/home/zglass/Documents/Projects/ArtLike-e8a8d5e75805.json"
   }
-
-
 
 
 def main(args):
@@ -16,7 +16,22 @@ def main(args):
     password = args.password
     firebase = pyrebase.initialize_app(config)
     auth = firebase.auth()
-    user = user.sign_in_with_email_and_password(email, password)
+    _user = auth.sign_in_with_email_and_password(email, password)
+    db = firebase.database()
+    all_users = db.child('users').get()
+    #loop through users, acess child works
+    for user in all_users.each():
+        use_key = user.key()
+        works = db.child('users').child(use_key).child('Works').get()
+        try:
+            for work in works.each():
+                db.child('users').child(use_key).child('Works').child(work.key()).update({"sold":"false"},
+                                                                                         _user['idToken'])
+        except:
+            tb = traceback.format_exc()
+            print(tb)
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
